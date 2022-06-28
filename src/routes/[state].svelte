7c2a3db205fe1745452f7e827441763ddc982a4c
Saltspeddy <walkercoco30@gmail.com>
2022-06-28 21:34:48 +0300

    3-components

--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -1,10 +1,15 @@
+<script context="module">
+    export async function preload(page){
+        return { state: page.params['state']};
+    }
+</script>
 <script>
     import CovidStat from "../components/CovidStat.svelte";
     import CovidChart from "../components/CovidChart.svelte";
     import TableContainer from "../components/TableContainer.svelte";
+    export let state;
 </script>
-
 <CovidStat />
 <CovidChart />
-<TableContainer />
 
+<h1>{state}</h1>
